f55059c9c91a8680460911a08a835ea1aba51426
Arthur <arthur.calado@ufpe.br>
2023-12-03 13:52:55 -0300

    docs: atualizado alguns comentários

--- client.py
+++ client.py
@@ -8,12 +8,14 @@ def main():
     # Verifica se conectou com sucesso
     if isinstance(c, socket.socket):
         
-        # Código para o algoritmo de aprendizado
-        # Ações possíveis:
-        # "left" = Girar para a Esquerda
-        # "right" = Girar para a Direita
-        # "jump" = Pular para a Frente        
+        # Nosso código para o algoritmo de aprendizado deve ficar aqui
+            # Ações possíveis:
+            # "left" = Girar para a Esquerda
+            # "right" = Girar para a Direita
+            # "jump" = Pular para a Frente        
         print("Conectado com sucesso")
+        
+        # Exemplo de como efetuar ação e pegar o estado e recompensa
         estado, recompensa = cn.get_state_reward(c, "jump")
     else:
         print("Falha na conexão")
