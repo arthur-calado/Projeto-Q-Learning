f139ec9f6edba87f9c3f97df759b899086696568
Arthur <arthur.calado@ufpe.br>
2023-12-03 13:48:17 -0300

    feat: adicionada verificação de conexão

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
-#Aqui vocês irão colocar seu algoritmo de aprendizado
+import connection as cn
+import socket
+def main():
+    
+    # Inicializa a conexão
+    c = cn.connect(2037)
+       
+    # Verifica se conectou com sucesso
+    if isinstance(c, socket.socket):
+        
+        # Código para o algoritmo de aprendizado
+        # Ações possíveis:
+        # "left" = Girar para a Esquerda
+        # "right" = Girar para a Direita
+        # "jump" = Pular para a Frente        
+        print("Conectado com sucesso")
+        estado, recompensa = cn.get_state_reward(c, "jump")
+    else:
+        print("Falha na conexão")
+main()
